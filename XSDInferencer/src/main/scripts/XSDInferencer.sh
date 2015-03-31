dad99a39fb461316d2d549ffc38bef18c33133c6
Pablo Alonso Rodriguez <pablo.alonso@ext.produban.com>
2015-03-31 09:30:16 +0200

    Minor fix in a unit test. Minor fix in linux Bash script

--- XSDInferencer/src/main/scripts/XSDInferencer.sh
+++ XSDInferencer/src/main/scripts/XSDInferencer.sh
@@ -2,4 +2,4 @@
 #Max heap size. At least 1024MB is recommended.
 MEM_MAX=1024
 #0.0.1-SNAPSHOT will be replaced during maven build
-java -Xmx${MEM_MAX} -jar XSDInferencer-JAR_VERSION.jar "$@"
+java -Xmx${MEM_MAX}m -jar XSDInferencer-JAR_VERSION.jar "$@"
